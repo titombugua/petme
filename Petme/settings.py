801bc5017d551978033c3aecec89f5c00beb450b
julzmbugua <julzmbugua@gmail.com>
2018-11-14 16:18:09 +0400

    hosts

--- Petme/settings.py
+++ Petme/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sm@g)(fbwdh5wc*xe@j++m9rh^uza5se9a57c5ptwkg*b@ki0x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://app-petme.herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["https://app-petme.herokuapp.com", "localhost", "https://127.0.0.1"]
 
 CORS_ORIGIN_ALLOW_ALL=True
 # CORS_ALLOW_CREDENTIALS = False
